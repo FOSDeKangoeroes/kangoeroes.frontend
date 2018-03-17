00b345a311c5e28c2e67ae37c6d341a96d3e7cd8
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-03-17 12:35:47 +0100

    Forbidden page uitwerken

--- frontend/app/components/app-forbidden/app-forbidden.component.ts
+++ frontend/app/components/app-forbidden/app-forbidden.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-app-forbidden',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppForbiddenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }

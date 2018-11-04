f4ee89a3a814754d8a7f4e1ffb332bb3d26da2c2
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-11-04 18:38:06 +0100

    Add environment variables to environment.ts in lib

---
+++ projects/kangoeroes-frontend-core/src/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    appUrl: '',
+    AUTH0_CLIENTID: ``,
+    AUTH0_DOMAIN: ``,
+    AUTH0_AUDIENCE: ``,
+    AUTH0_REDIRECTURI: ``,
+    baseUrl: ``,
+    AUTH0_SCOPES: '',
+    AUTH0_RESPONSETYPE: ''
+};
--- projects/kangoeroes-frontend-core/src/lib/auth/guards/authentication.guard.ts
+++ projects/kangoeroes-frontend-core/src/lib/auth/guards/authentication.guard.ts
@@ -4,8 +4,9 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import decode from 'jwt-decode';
-import { AuthService } from 'projects/kangoeroes-frontend-leidingbeheer/src/app/core/auth/auth.service';
+
 import { KangoeroesAuthModule } from '../kangoeroes-auth.module';
+import { AuthService } from 'projects/kangoeroes-frontend-core/src/lib/auth/services/auth.service';
 
 
 @Injectable({ 

cbe857ff904b865ce7728af1c1fabedda93ee85e
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-11-22 16:17:58 +0100

    Add takDataService and refactor

---
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/tak/shared/tak-data.service.ts
@@ -0,0 +1,18 @@
+import { TakSerializer } from './tak-serializer';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ResourceService } from 'projects/kangoeroes-frontend-core/src/lib/data-service/resource-service';
+import { Tak } from './tak.model';
+import { ConfigService } from 'projects/kangoeroes-frontend-core/src/lib/config/config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TakDataService extends ResourceService<Tak> {
+
+  constructor(httpClient: HttpClient, configService: ConfigService) {
+
+    const url = `${configService.get().appUrl}/api`
+    super(httpClient,url,'tak', new TakSerializer())
+   }
+}

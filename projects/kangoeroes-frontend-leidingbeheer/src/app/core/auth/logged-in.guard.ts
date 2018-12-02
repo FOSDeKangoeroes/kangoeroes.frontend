b5a9ead7e84b3aea872b80acde13beceb8b3d7d0
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-12-02 11:56:38 +0100

    More housekeeping

--- projects/kangoeroes-frontend-leidingbeheer/src/app/core/auth/logged-in.guard.ts
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/core/auth/logged-in.guard.ts
@@ -1,25 +1,25 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from 'projects/kangoeroes-frontend-core/src/lib/auth/services/auth.service';
 
-
-
-@Injectable(
-  {
-    providedIn: 'root'
-  }
-)
+@Injectable({
+  providedIn: 'root'
+})
 export class LoggedInGuard implements CanActivate {
-
   constructor(private authService: AuthService) {}
+
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-
-      if (this.authService.isAuthenticated()) {
-          return true;
-      }
-  this.authService.login();
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isAuthenticated()) {
+      return true;
+    }
+    this.authService.login();
   }
 }

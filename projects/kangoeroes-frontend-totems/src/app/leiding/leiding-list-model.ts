0b7d1b5d03ba0ea540b86b809a9f841584de89f3
Thomas de Wulf <thomas.de.wulf@telenet.be>
2018-12-16 12:21:51 +0100

    Remove unused model

--- projects/kangoeroes-frontend-totems/src/app/leiding/leiding-list-model.ts
+++
@@ -1,8 +0,0 @@
-
-import { Leiding } from './leiding.model';
-import { Pagination } from 'projects/kangoeroes-frontend-leidingbeheer/src/app/models/pagination-model';
-
-export interface LeidingList {
-    leiding: Leiding[];
-    pagination: Pagination;
-}

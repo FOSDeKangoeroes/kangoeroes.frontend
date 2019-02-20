dbb417845764b629a88410997b19fa12b0322bb8
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-02-20 21:53:11 +0100

    Start adding drinks

---
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/poef/drank-type/components/drank-type-add/drank-type-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drank-type-add',
+  templateUrl: './drank-type-add.component.html',
+  styleUrls: ['./drank-type-add.component.scss']
+})
+export class DrankTypeAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

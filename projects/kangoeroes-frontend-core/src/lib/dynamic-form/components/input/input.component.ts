b87cc17dc96ed88d902d3bc0e865cf34f35802f3
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-02-26 19:55:13 +0100

    Final dynamic forms implementation

--- projects/kangoeroes-frontend-core/src/lib/dynamic-form/components/input/input.component.ts
+++ projects/kangoeroes-frontend-core/src/lib/dynamic-form/components/input/input.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FieldComponentBase } from '../field-component-base';
+import { FieldConfig } from '../../field.interface';
 @Component({
   selector: 'kangoeroe-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
 export class InputComponent extends FieldComponentBase implements OnInit {
+
   constructor() {
     super();
   }
--- projects/kangoeroes-frontend-leidingbeheer/src/app/poef/poef.module.ts
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/poef/poef.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PoefRoutingModule } from './poef-routing.module';
 import { DrankModule } from './drank/drank.module';
+import { DrankTypeModule } from './drank-type/drank-type.module';
 
 @NgModule({
   imports: [
     CommonModule,
     PoefRoutingModule,
-    DrankModule
+    DrankModule,
+    DrankTypeModule
   ]
 })
 export class PoefModule {}

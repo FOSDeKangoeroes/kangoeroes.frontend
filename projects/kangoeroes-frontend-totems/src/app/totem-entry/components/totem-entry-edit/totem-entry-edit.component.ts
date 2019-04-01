1d13c75017aa7d026ddcc33dc61b68cd175f424b
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-04-01 09:47:42 +0200

    Fix issue #37 - Voorouder veld wordt niet correct aangepast

--- projects/kangoeroes-frontend-totems/src/app/totem-entry/components/totem-entry-edit/totem-entry-edit.component.ts
+++ projects/kangoeroes-frontend-totems/src/app/totem-entry/components/totem-entry-edit/totem-entry-edit.component.ts
@@ -1,6 +1,12 @@
 import { MatSnackBar } from '@angular/material';
 import { TotemEntryService } from '../../shared/totem-entry.service';
-import { Component, OnInit, Input, ViewChild, AfterViewInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
 import { TotemEntry } from '../../shared/totem-entry-model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { LeidingDataService } from '../../../leiding/leiding-data.service';
@@ -9,6 +15,7 @@ import { AnimalDataService } from '../../../totemanimal/shared/animal-data.servi
 import { TotemEntryDataService } from '../../shared/totem-entry-data.service';
 import * as moment from 'moment';
 import { RequireMatch } from 'projects/kangoeroes-frontend-core/src/lib/validators/autocomplete-validator';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-totem-entry-edit',
@@ -16,16 +23,15 @@ import { RequireMatch } from 'projects/kangoeroes-frontend-core/src/lib/validato
   styleUrls: ['./totem-entry-edit.component.scss']
 })
 export class TotemEntryEditComponent implements OnInit {
-
   @Input() totemEntry: TotemEntry;
 
   addEntryFormGroup: FormGroup;
   private voorouder?: TotemEntry;
 
   displayFnGrandparent = (x: any) => {
- if (x) {
-   return `${x.displayName} \/\/ ${x.leidingVoornaam} ${x.leidingNaam}`;
- }
+    if (x) {
+      return `${x.displayName} \/\/ ${x.leidingVoornaam} ${x.leidingNaam}`;
+    }
   }
 
   constructor(
@@ -35,25 +41,31 @@ export class TotemEntryEditComponent implements OnInit {
     public animalDataService: AnimalDataService,
     public totemEntryDataService: TotemEntryDataService,
     private totemEntryService: TotemEntryService,
-    private snackbar: MatSnackBar
-  ) {}
+    private snackbar: MatSnackBar,
+    private router: Router
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
 
   ngOnInit() {
-
+    console.log('init called');
     this.addEntryFormGroup = this.fb.group({
-      adjectief: [this.totemEntry.adjectief, [Validators.required, RequireMatch]],
+      adjectief: [
+        this.totemEntry.adjectief,
+        [Validators.required, RequireMatch]
+      ],
       totem: [this.totemEntry.totem, [Validators.required, RequireMatch]],
       datumGekregen: [''],
       voorouder: [[RequireMatch]]
-
     });
 
-    if  (this.totemEntry.voorouderId) {
-      this.totemEntryDataService.read(this.totemEntry.voorouderId).subscribe(res => {
-        this.addEntryFormGroup.get('voorouder').setValue(res);
-      });
+    if (this.totemEntry.voorouderId) {
+      this.totemEntryDataService
+        .read(this.totemEntry.voorouderId)
+        .subscribe(res => {
+          this.addEntryFormGroup.get('voorouder').setValue(res);
+        });
     }
-
   }
 
   addFormControl(name: string, formGroup: FormGroup): void {
@@ -61,21 +73,26 @@ export class TotemEntryEditComponent implements OnInit {
   }
 
   onSubmit() {
-    const datumGekregen = moment(this.addEntryFormGroup.value.datumGekregen).toISOString();
-    const voorouder = this.addEntryFormGroup.value.voorouder.id ? this.addEntryFormGroup.value.voorouder.id : 0;
+    const datumGekregen = moment(
+      this.addEntryFormGroup.value.datumGekregen
+    ).toISOString();
+    const voorouder = this.addEntryFormGroup.value.voorouder.id
+      ? this.addEntryFormGroup.value.voorouder.id
+      : 0;
     const newEntry = {
-
       totemId: this.addEntryFormGroup.value.totem.id,
       adjectiefId: this.addEntryFormGroup.value.adjectief.id,
       datumGegeven: datumGekregen ? datumGekregen : undefined,
       voorouderId: voorouder
     };
 
-    this.totemEntryDataService.update(newEntry, this.totemEntry.id).subscribe(res => {
-      this.totemEntryService.entryChanged$.emit();
-      this.snackbar.open(`${res.displayName} werd gewijzigd!`, null, {
-        duration: 2000
+    this.totemEntryDataService
+      .update(newEntry, this.totemEntry.id)
+      .subscribe(res => {
+        this.totemEntryService.entryChanged$.emit();
+        this.snackbar.open(`${res.displayName} werd gewijzigd!`, null, {
+          duration: 2000
+        });
       });
-    });
   }
 }

bdbfc054a10394433e0c29fe6c2e323f6ea0ce93
Thomas de Wulf <thomas.de.wulf@telenet.be>
2019-08-17 11:05:11 +0200

    Drank verwijderen

--- projects/kangoeroes-frontend-leidingbeheer/src/app/poef/drank/pages/drank-detail/drank-detail.component.ts
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/poef/drank/pages/drank-detail/drank-detail.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Drank } from '../../shared/drank.model';
+import { DrankDataService } from '../../shared/drank-data.service';
+import { SnotifyService } from 'ng-snotify';
 
 @Component({
   selector: 'app-drank-detail',
@@ -11,10 +13,25 @@ export class DrankDetailComponent implements OnInit {
 
   drank: Drank;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private drankDataService: DrankDataService,
+    private snotifyService: SnotifyService,
+    private router: Router) { }
 
   ngOnInit() {
     this.route.data.subscribe(item => this.drank = item['drank']);
+  }
+
+  delete() {
+    if(confirm(`Wil je ${this.drank.displayName} verwijderen?`)) {
+      this.drankDataService.delete(this.drank.id).subscribe(res => {
+        this.router.navigate(['/poef/drank']);
+        this.snotifyService.success('Drank werd verwijderd.');
+      }, err => {
+        this.snotifyService.error('Kan drank niet verwijderen.');
+      });
+    }
   }
 
 }

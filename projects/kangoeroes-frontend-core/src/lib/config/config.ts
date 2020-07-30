ed0852d4dd080bf98c7087e312d41328b6049040
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-07-30 19:05:40 +0200

    Add app insights

--- projects/kangoeroes-frontend-core/src/lib/config/config.ts
+++ projects/kangoeroes-frontend-core/src/lib/config/config.ts
@@ -1,13 +1,13 @@
 export interface Config {
-    production: boolean;
-    appUrl: string;
-    auth0ClientId: string;
-    auth0Domain: string;
-    auth0Audience: string;
-    auth0RedirectUri: string;
-    auth0Scopes: string;
-    auth0ResponseType: string;
-    baseUrl: string;
-    postAuthUrl: string;
-
+  production: boolean;
+  appUrl: string;
+  auth0ClientId: string;
+  auth0Domain: string;
+  auth0Audience: string;
+  auth0RedirectUri: string;
+  auth0Scopes: string;
+  auth0ResponseType: string;
+  baseUrl: string;
+  postAuthUrl: string;
+  appInsightsInstrumentationKey: string;
 }

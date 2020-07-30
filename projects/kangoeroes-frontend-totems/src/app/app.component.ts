bd34dee045903c01d85401b714972d99f269a55d
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-07-30 19:48:07 +0200

    Start app insights on AppComponent load

--- projects/kangoeroes-frontend-totems/src/app/app.component.ts
+++ projects/kangoeroes-frontend-totems/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../kangoeroes-frontend-core/src/lib/auth/services/auth.service';
 import { gitVersion } from 'projects/kangoeroes-frontend-core/src/lib/config/git-version';
+import { MonitoringService } from 'projects/kangoeroes-frontend-core/src/lib/monitoring/monitoring.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,7 @@ export class AppComponent {
   title = 'app';
   version: string;
 
-  constructor(private auth: AuthService) {
+  constructor(private auth: AuthService, private monitoringService: MonitoringService) {
     this.auth.handleAuthentication();
 
     this.version = `${gitVersion.branch}-${gitVersion.commit}`;

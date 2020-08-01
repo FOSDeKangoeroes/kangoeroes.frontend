70d31837d5068e78178f66ffde8406b8cd710713
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-08-01 17:48:10 +0200

    Add poef status to settings page

--- projects/kangoeroes-frontend-leidingbeheer/src/app/poef/settings/pages/settings/settings.component.ts
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/poef/settings/pages/settings/settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TakDataService } from 'projects/kangoeroes-frontend-leidingbeheer/src/app/tak/shared/tak-data.service';
+import { Tak } from 'projects/kangoeroes-frontend-leidingbeheer/src/app/tak/shared/tak.model';
+import { QueryOptions } from 'projects/kangoeroes-frontend-core/src/lib/data-service/query-options';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsComponent implements OnInit {
 
-  constructor() { }
+  public takken: Tak[];
+
+  columnsToDisplay = [
+   'name', 'tabIsAllowed'
+  ];
+
+  constructor(public takDataService: TakDataService) { }
 
   ngOnInit() {
+    const queryOptions = new QueryOptions();
+    queryOptions.pageSize = 100;
+    queryOptions.sortBy = 'volgorde';
+    this.takDataService.list(queryOptions).subscribe(res => {
+      this.takken = res.body;
+    });
+  }
+
+  toggleTabAllowed(tak: Tak) {
+    tak.tabIsAllowed = !tak.tabIsAllowed;
+
+    this.takDataService.update(tak, tak.id).subscribe(res => {
+      console.log('updated!');
+    });
   }
 
 }
--- projects/kangoeroes-frontend-leidingbeheer/src/app/poef/settings/settings.module.ts
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/poef/settings/settings.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { SettingsRoutingModule } from './settings-routing.module';
+import { AgGridModule } from 'ag-grid-angular';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [SettingsComponent],
   imports: [
     CommonModule,
-    SettingsRoutingModule
+    SettingsRoutingModule,
+    MatTableModule,
+    MatSlideToggleModule,
   ]
 })
 export class SettingsModule { }

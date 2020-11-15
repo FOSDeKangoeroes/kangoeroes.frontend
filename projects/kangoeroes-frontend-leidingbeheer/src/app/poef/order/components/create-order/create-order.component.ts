309fabc165bbda0c0c73d6a67c6ea199dc775602
Thomas de Wulf <thomas.de.wulf@telenet.be>
2020-11-15 17:57:53 +0100

    Fix ref

--- projects/kangoeroes-frontend-leidingbeheer/src/app/poef/order/components/create-order/create-order.component.ts
+++ projects/kangoeroes-frontend-leidingbeheer/src/app/poef/order/components/create-order/create-order.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 import { FieldConfig } from 'projects/kangoeroes-frontend-core/src/lib/dynamic-form/field.interface';
 import { LeidingDataService } from 'projects/kangoeroes-frontend-leidingbeheer/src/app/leiding/shared/leiding-data.service';
 import { DrankDataService } from '../../../drank/shared/drank-data.service';
@@ -13,7 +14,11 @@ export class CreateOrderComponent implements OnInit {
 
   orderForm: FormGroup;
 
-  constructor(private fb: FormBuilder, public leidingDataService: LeidingDataService,public drankDataService: DrankDataService) {
+  constructor(
+    private fb: FormBuilder,
+    public leidingDataService: LeidingDataService,
+    public drankDataService: DrankDataService,
+    public addOrderRef: BsModalRef) {
 
     this.orderForm = this.fb.group({
       orderedFor: [, [Validators.required]],
